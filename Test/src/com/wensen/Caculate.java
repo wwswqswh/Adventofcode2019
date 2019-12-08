6f4e757c92e17d3605decff666f4c5d3fa6a36c1
wwswqswh <wwswqswh@163.com>
2019-12-08 09:06:08 +0100

    Add files via upload

---
+++ Test/src/com/wensen/Caculate.java
@@ -0,0 +1,24 @@
+package com.wensen;
+
+public class Caculate {
+
+	int compute(int mass){
+		return compute(mass, 0);
+	}
+	
+	int compute(int massToCalculate, int sum){
+		int fuel = Math.floorDiv(massToCalculate, 3) - 2;
+		if (fuel < 1) {			
+			return sum;
+		}
+		else {
+			sum += fuel;
+			return compute(fuel, sum);
+		}
+	}
+	
+	public static void main(String[] args) {
+		Caculate cal = new Caculate();
+		System.out.println(cal.compute(100756));
+	}
+}
